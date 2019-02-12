ac6d24082d93ef323201b85649fd228a64cc8cad
Thiago Fontes <thiago93br@gmail.com>
2019-02-12 14:12:53 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FinancialSystem.Mixfile do
     [
       app: :financial_system,
       version: "0.1.1",
-      elixir: "~> 1.9.0-dev",
+      elixir: "~> 1.8.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
